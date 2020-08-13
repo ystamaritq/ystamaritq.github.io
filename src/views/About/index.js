e9317050dfcc5c0255d1d8762a23b129e8fa4b17
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-12 22:34:51 -0500

    added last changes

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -12,21 +12,23 @@ import {
 } from "mdbreact";
 import pic from "./imgs/yady.png";
 import { Container, Col, Row } from "react-bootstrap";
+import "./style.css";
 
 const About = () => {
 	return (
 		<Container fluid className="p-5">
 			<MDBRow className="d-flex justify-content-center flex-wrap flex-row">
 				<MDBCol className="col-lg-8 col-md-6 col-sm-12">
-					<MDBCard className="bg-light">
+					<MDBCard className="bg-light bg-border">
 						<MDBCardImage
-							className="mb-0 py-2 rounded-circle z-depth-1 img-fluid col-sm-12 mx-auto d-lg-flex"
+							className="mb-0 py-3 rounded-circle z-depth-1 img-fluid col-sm-12 mx-auto d-lg-flex d-md-flex d-sm-flex"
 							src={pic}
 							md="4"
 							alt="me"
 							style={{ width: "22em" }}
 						/>
-						<hr className="mt-0" />
+
+						<hr className="mt-0 bg-light" />
 						<MDBCardBody cascade className="text-center">
 							<MDBCardTitle className="card-title mb-4">
 								<strong>About Me</strong>
@@ -36,7 +38,7 @@ const About = () => {
 								Full Stack Developer
 							</p>
 
-							<MDBCardText className="text-justify">
+							<MDBCardText className="text-justify custom-p-color">
 								My name is Yadira Tamarit. I’m originally from Cuba. I’m a mom
 								of two wonderful boys.I have a Bachelor Degree in Computer
 								Science. I am currently enrolled in a UT Coding Bootcamp(The
@@ -44,7 +46,7 @@ const About = () => {
 								order to learn all new technologies and continue growing my
 								career.
 							</MDBCardText>
-							<MDBCardText className="text-justify">
+							<MDBCardText className="text-justify custom-p-color">
 								I love mathematics for that reason I choose computer science as
 								a career. Professionally, I have always liked to challenge
 								myself by never giving up on any difficult task. I also consider
@@ -52,7 +54,7 @@ const About = () => {
 								dedicated, hard-worker, highly organized and able to work under
 								pressure.
 							</MDBCardText>
-							<MDBCardText className="text-justify">
+							<MDBCardText className="text-justify custom-p-color">
 								I love studying and I’m open to learn when is need it. I studied
 								Computer Sciences in (Universidad de las Ciencias Informaticas)
 								located in Havana, Cuba from (2003 - 2008).For more information
